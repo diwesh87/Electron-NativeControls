053fc19c7ccee5a9a36922ccf6626981909805a1
Diwesh Saxena <diweshsaxena@gmail.com>
2016-09-26 10:21:52 +0900

    Added tray icon demo

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-const electron = require('electron');
+const electron = require('electron'); //Bring in the electron module
+const path = require('path'); //Bring in the path module for basic system operations
 
 const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-const Menu = electron.Menu
+const BrowserWindow = electron.BrowserWindow // Call the BrowserWindow from electron module
+const Menu = electron.Menu // Call the Menu from electron module
+
+const Tray = electron.Tray // Call the Tray from electron module
+
 
 app.on('ready', _ => {
   new BrowserWindow()
-  const name = electron.app.getName()
+  const tray = new Tray(path.join('src', 'trayIcon.png')) // Instantiate a new Tray with passing an argument(this case a tray icon)
+  const name = electron.app.getName() //Get the app name from Package.json (Product Name)
+  //Define a template for a Custom Menu
   const template = [
     {
       label: name,
@@ -18,6 +24,17 @@ app.on('ready', _ => {
       ]
   }
 ]
+// End of Custom Menu
+
   const menu = Menu.buildFromTemplate(template)
   Menu.setApplicationMenu(menu)
+
+//Define a contextMenu for system tray icon
+const contextMenu = Menu.buildFromTemplate([
+  { label: `More Options`, click: _ => {console.log('Clicked Tray Icon');}},
+])
+//End of trayIcon control
+
+tray.setContextMenu(contextMenu)
+tray.setToolTip(name)
 })
